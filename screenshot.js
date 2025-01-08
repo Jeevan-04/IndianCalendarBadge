42ad8b4dd50b87f68cc6e18ff80e004bea605d57
Jeevan Naidu <142775323+Jeevan-04@users.noreply.github.com>
2025-01-08 20:17:13 +0530

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -9,26 +9,27 @@ const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
-    console.log('Clearing cache and cookies...');
-    await page.deleteCookie(...await page.cookies());
+    console.log('Setting user-agent...');
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
+
+    console.log('Disabling cache...');
     await page.setCacheEnabled(false);
 
     console.log('Setting viewport...');
     await page.setViewport({
       width: 1200,
       height: 800,
-      deviceScaleFactor: 2,
+      deviceScaleFactor: 2
     });
 
     console.log('Navigating to the page...');
-    const currentTimestamp = new Date().getTime();
-    await page.goto(`https://vikramsamvat.onrender.com?cache_bust=${currentTimestamp}`, {
-      waitUntil: 'networkidle0',
+    await page.goto('https://vikramsamvat.onrender.com', {
+      waitUntil: 'networkidle2',
       timeout: 120000,
     });
 
-    console.log('Waiting for content to update...');
-    await wait(5000); // Wait for 5 seconds to allow dynamic content to update
+    console.log('Waiting for dynamic content to load...');
+    await wait(5000); // Wait for 5 seconds to allow dynamic content to load
 
     console.log('Waiting for #badge element...');
     await page.waitForSelector('#badge', { timeout: 120000 });
